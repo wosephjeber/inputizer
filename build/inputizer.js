a4147718c8ca496e24c2007cb96606811bfeccf3
Joe Weber <wosephjeber@gmail.com>
2015-02-16 23:05:45 -0600

    Make sure callback is a function

--- build/inputizer.js
+++ build/inputizer.js
@@ -7,6 +7,9 @@
   $.fn.inputizer = function(onBlur) {
     return this.each(function() {
       
+      console.log(typeof onBlur);
+      onBlur = (typeof onBlur === 'function') ? onBlur : function() {};
+      
       $(this).attr('contenteditable', true).addClass('inputized');
       
       $(this).bind('blur', function() {onBlur.call(this)});
