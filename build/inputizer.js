ed0c6fc03d43327464fbe28d8b96da8f4c12d3d5
Joe Weber <wosephjeber@gmail.com>
2015-02-16 23:03:07 -0600

    Building out plugin

--- build/inputizer.js
+++ build/inputizer.js
@@ -4,4 +4,13 @@
         
     };
  
+  $.fn.inputizer = function(onBlur) {
+    return this.each(function() {
+      
+      $(this).attr('contenteditable', true).addClass('inputized');
+      
+      $(this).bind('blur', function() {onBlur.call(this)});
+      
+    })
+  };
 }( jQuery ));
