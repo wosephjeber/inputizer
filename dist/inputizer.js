d2b3e6a35848e486239a352efff7434cc015d0ef
Joe Weber <wosephjeber@gmail.com>
2015-03-03 22:13:22 -0600

    Something's up with the minified js file

---
+++ dist/inputizer.js
@@ -0,0 +1,48 @@
+(function ( $ ) {
+  $.fn.selectText = function(){
+    var doc = document;
+    var element = this[0];
+    var range;
+    if (doc.body.createTextRange) {
+      range = document.body.createTextRange();
+      range.moveToElementText(element);
+      range.select();
+    } else if (window.getSelection) {
+      var selection = window.getSelection();        
+      range = document.createRange();
+      range.selectNodeContents(element);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  };
+  
+  $.fn.inputizer = function(onBlur) {
+    return this.each(function() {
+      
+      onBlur = (typeof onBlur === 'function') ? onBlur : function() {};
+      
+      var _this = this;
+      var $this = $(this);
+      
+      $this.attr('contenteditable', true).addClass('inputized');
+      
+      $this.bind('blur', function() {
+        onBlur.call(_this, $this.html());
+      });
+      
+      $this.keypress(function(e){
+        var regex = /[^0-9\,]/;
+        var k = e.which;
+        var keychar = String.fromCharCode(k);
+        if (k == 13) {
+          $(this).trigger('blur');
+          return false;
+        }
+      });
+      
+      $this.on("click focus", function(){
+        $this.selectText();
+      });
+    });
+  };
+}( jQuery ));
